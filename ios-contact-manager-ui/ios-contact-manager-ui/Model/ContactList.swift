ec0112fe7848661a75f225f8d0f4b7a8eb9ce137
jus1234 <osjamg1996@gmail.com>
2024-01-02 21:30:23 +0900

    feat: ContactList 구조체 생성

---
+++ ios-contact-manager-ui/ios-contact-manager-ui/Model/ContactList.swift
@@ -0,0 +1,16 @@
+//
+//  ContactList.swift
+//  ios-contact-manager-ui
+//
+//  Created by 장우석 on 1/2/24.
+//
+
+import Foundation
+
+struct ContactList {
+    private var contactList: Array<Contact>
+    
+    init() {
+        self.contactList = []
+    }
+}
